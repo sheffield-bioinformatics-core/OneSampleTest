e57d49a63d5a559f43a7e36f59f825070cd7ddab
markdunning <mark.dunning@gmail.com>
2016-05-24 14:16:39 +0100

    update install script

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
-install.packages(c("shiny","ggplot2","reshape2","gridExtra","RcmdrMisc","knitr"))
+install.packages(c("shiny","ggplot2","reshape2","gridExtra","knitr"))
