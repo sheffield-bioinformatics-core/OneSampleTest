57a40cd203861f516b2807d70a93ae209f5e8ab6
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-02 12:44:48 +0100

    Removed --rm flag as not compatible with -d in some versions of docker.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p logs
 chmod ugo+rwx logs
-docker run -u shiny -d --rm -p 80:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/one-sample-test
+docker run -u shiny -d -p 80:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/one-sample-test
