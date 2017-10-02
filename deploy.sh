30c56aed9a385f4bf954423fb735aa75a8eacf64
eldrid01 <matthew.d.eldridge@gmail.com>
2017-10-02 10:48:52 +0100

    Removed sleep and docker ps

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 mkdir -p logs
 chmod ugo+rwx logs
-docker run -u shiny -d --rm -p 80:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/one-sample-test:latest
-sleep 2
-docker ps -a
+docker run -u shiny -d --rm -p 80:3838 -v ${PWD}/logs:/var/log/shiny-server crukci-bioinformatics/one-sample-test
